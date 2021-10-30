d735f0e08e0ffd391527150335959f03a855f215
Flavio <flavionogueirabarros@gmail.com>
2021-10-30 17:56:07 -0300

    add sendbox and send button

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,7 +1,25 @@
+import styles from './../styles/Chat.module.css'
+import { Input } from 'semantic-ui-react'
 export default function Chat(){
     return (
-        <div>
-            <h1>chat</h1>
+        <div className={styles.dialogbox}>
+            <div className={styles.dialogs}>
+                <h1>dialogs</h1>
+            </div>
+            <div className={styles.messageboxsender}>
+                <div className={styles.inputbox}>
+                    <Input fluid placeholder='Digite uma mensagem'/>
+                </div>
+                <div className={styles.sendbuttonbox}>
+                    <button className={styles.sendbutton}>
+                        <svg viewBox="0 0 24 24" width='24' height='24'>
+                            <path fill="#666" d='M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z' >
+
+                            </path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
